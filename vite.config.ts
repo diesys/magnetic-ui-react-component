8c4f3f58f866dfdce4232a7f33f5d76a28409adb
diesys <turingsbite@gmail.com>
2026-03-22 03:35:22 +0100

    ci: add check&deploy GitHub actions + drop `gh-pages`

--- vite.config.ts
+++ vite.config.ts
@@ -4,22 +4,21 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  // Base for hosting
-  // base: "/magnetic-ui-react-component",
-	base: "/",
-	plugins: [
-		// The React and Tailwind plugins are both required for Make, even if
-		// Tailwind is not being actively used – do not remove them
-		react(),
-		tailwindcss(),
-	],
-	resolve: {
-		alias: {
-			// Alias @ to the src directory
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
+  // Base for GitHub pages
+  base: "/magnetic-ui-react-component",
+  plugins: [
+    // The React and Tailwind plugins are both required for Make, even if
+    // Tailwind is not being actively used – do not remove them
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      // Alias @ to the src directory
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 
-	// File types to support raw imports. Never add .css, .tsx, or .ts files to this.
-	assetsInclude: ["**/*.svg", "**/*.csv"],
+  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
+  assetsInclude: ["**/*.svg", "**/*.csv"],
 });
